088a6a14c89060b02f642429c1a664ba94b27c60
mykhaylo.kubasov <hsmishka@gmail.com>
2014-02-09 20:42:30 -0800

    test commit

--- fluffypages/src/com/fluffypages/server/admin/AdminServletModule.java
+++ fluffypages/src/com/fluffypages/server/admin/AdminServletModule.java
@@ -5,5 +5,6 @@ import com.google.inject.servlet.ServletModule;
 public class AdminServletModule extends ServletModule {
   @Override protected void configureServlets() {
     serve("/uploadPictures").with(UploadPicturesServlet.class);
+
   }
 }
